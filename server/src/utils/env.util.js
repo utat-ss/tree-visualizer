e9109638746e59bb1186edbdb5abbccdb5141217
jdtech3 <dm.joedai@gmail.com>
2023-01-17 18:42:34 +0000

    Fix getEnv error msg

--- server/src/utils/env.util.js
+++ server/src/utils/env.util.js
@@ -1,11 +1,11 @@
 var getEnv = function(envVar) {
-    env = process.env[envVar];
-    if (env !== null && env !== undefined) {
-        return env;
-    }
-    else {
-        throw new Error("NOTION_CLIENT environment variable missing");
-    }
+  env = process.env[envVar];
+  if (env !== null && env !== undefined) {
+    return env;
+  }
+  else {
+    throw new Error(envVar + " environment variable missing!");
+  }
 }
 
 module.exports = { getEnv };
