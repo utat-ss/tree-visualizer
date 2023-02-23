2f11bc6aea5f14c6a50ec05d4ed2da66a51a3b63
David Maranto <34867698+DM1122@users.noreply.github.com>
2023-02-23 03:02:58 +0000

    Add sample data to story

--- client/src/app/components/org-chart/org-chart.component.stories.ts
+++ client/src/app/components/org-chart/org-chart.component.stories.ts
@@ -1,15 +1,26 @@
 import { Meta, Story } from '@storybook/angular';
 import { OrgChartComponent } from './org-chart.component';
+import { action } from '@storybook/addon-actions';
+import * as go from 'gojs';
 
 export default {
-  component: OrgChartComponent,
+    component: OrgChartComponent,
 } as Meta;
 
 const Template: Story = (args) => ({
-  props:args,
+    props: args,
 });
 
-export const FirstStory = Template.bind({});
-FirstStory.args= {
+const model = new go.TreeModel([
+    { key: 1, name: 'Alpha', parent: '' },
+    { key: 2, name: 'Beta', parent: 1 },
+    { key: 3, name: 'Gamma', parent: 1 },
+    { key: 4, name: 'Delta', parent: 2 },
+    { key: 5, name: 'Epsilon', parent: 2 },
+]);
 
+export const FirstStory = Template.bind({});
+FirstStory.args = {
+    model,
+    nodeClicked: action('nodeClicked'),
 };
