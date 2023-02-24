6b9f0b2b0b78448d137fd927d03b830f61429d0f
David Maranto <34867698+DM1122@users.noreply.github.com>
2023-02-24 01:36:01 +0000

    Remove legacy graph component from app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
+import { HttpClientModule } from "@angular/common/http"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GraphComponent } from './graph/graph.component';
-import { OrgChartComponent } from './components/org-chart/org-chart.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { OrgChartComponent } from "./components/org-chart/org-chart.component"
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GraphComponent,
-    OrgChartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, OrgChartComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
