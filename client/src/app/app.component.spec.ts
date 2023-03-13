f247b2c6b430078f94022bdd03808c7e595fdde9
Joe Dai <dm.joedai@gmail.com>
2023-03-12 23:16:59 -0400

    Client - fix broken tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tree-visualizer app is running!');
+    expect(compiled.querySelector('p')?.textContent).toContain('This is TreeVisualizer!');
   });
 });
--- client/src/app/services/backend.service.spec.ts
+++ client/src/app/services/backend.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 import { BackendService } from './backend.service';
 
@@ -6,7 +7,11 @@ describe('BackendService', () => {
   let service: BackendService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(BackendService);
   });
 
