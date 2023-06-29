335374513718a58ce0f06e61b1cdb124ef064b44
Stephanie Lu <84485762+stephanieyflu@users.noreply.github.com>
2023-06-29 01:33:32 +0000

    Delete color property in app story

--- client/src/app/app.component.stories.ts
+++ client/src/app/app.component.stories.ts
@@ -36,7 +36,6 @@ const model: go.TreeModel = new go.TreeModel({
             description: "A description",
             rationale: "A rationale",
             url: "https://www.notion.so/UTAT-Team-A-74331bc1129c417c850a0a5ca0deb759",
-            color: "red"
         },
         {
             id: "123456789",
@@ -47,7 +46,6 @@ const model: go.TreeModel = new go.TreeModel({
             description: "Another description",
             rationale: "A rationale",
             url: "https://www.notion.so/Child-1-123456789",
-            color: "red"
         },
         {
             id: "abcdefghi",
@@ -59,7 +57,6 @@ const model: go.TreeModel = new go.TreeModel({
                 "A very long form description that may or may not exceed the width of the node. The quick brown fox jumps over the lazy dog. Octopuses have several tentacles. Icecream is cold. Did you know that lava is warm? Lava cake with an ice cream scoop on the side is delicious.",
             rationale: "A rationale",
             url: "https://www.notion.so/Child-2-abcdefghi",
-            color: "red"
         },
         {
             id: "qwertyuiop",
@@ -70,7 +67,6 @@ const model: go.TreeModel = new go.TreeModel({
             description: "Yet another description that is just a little bit longer",
             rationale: "A rationale",
             url: "https://www.notion.so/Grandchild-1-qwertyuiop",
-            color: "red"
         },
     ],
   });
