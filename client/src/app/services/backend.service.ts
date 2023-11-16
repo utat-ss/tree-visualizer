eba54e23c5d5fb6567e916a4200769534cf44d3e
jdtech3 <dm.joedai@gmail.com>
2023-11-16 20:43:00 +0000

    Client: add Docker server URL as separate environment

--- client/src/app/services/backend.service.ts
+++ client/src/app/services/backend.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RequirementsGraph } from '../interfaces/requirements-graph';
+import { environment } from '../..//environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,7 @@ export class BackendService {
   constructor(private http: HttpClient) {}
 
   getRequirementsGraph(): Observable<RequirementsGraph> {
-    // TODO: Define URL as env variable
-    // let url = "https://3000--main--tree-visualizer--jdtech3.code.j0e.ca/api/requirements";
-    let url = "http://localhost:3000/api/requirements"
+    let url = environment.apiBaseUrl + "/requirements"
 
     return this.http.get<RequirementsGraph>(url)
   }
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiBaseUrl: 'http://treeviz-server:3000/api'
+};
